21a986369aea3e2a4e38921ab6d30f1df639dea6
TarasVovk669 <taras.ivschk@gmail.com>
2024-07-03 21:07:49 +0200

    refactor

--- src/main/java/com/ebbinghaus/memory/app/bot/MemoryBot.java
+++ src/main/java/com/ebbinghaus/memory/app/bot/MemoryBot.java
@@ -9,9 +9,6 @@ import com.ebbinghaus.memory.app.domain.*;
 import com.ebbinghaus.memory.app.model.*;
 import com.ebbinghaus.memory.app.service.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
@@ -23,51 +20,22 @@ import org.telegram.telegrambots.meta.api.objects.message.Message;
 @Component
 public class MemoryBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
 
-  private static final Logger log = LoggerFactory.getLogger(MemoryBot.class);
-
   private final String token;
   private final String ownerName;
-
-  private QuizService quizService;
-  private UserService userService;
-  private ObjectMapper objectMapper;
-  private MessageService messageService;
-  private CategoryService categoryService;
-  private KeyboardService keyboardService;
-  private SchedulerService schedulerService;
-  private MessageSourceService messageSourceService;
-  private TelegramClientService telegramClientService;
-  private ChatMessageStateService chatMessageStateService;
-
+  private final UserService userService;
+  private final ObjectMapper objectMapper;
   private final TelegramBotService telegramBotService;
 
   public MemoryBot(
       @Value("${bot.token}") String token,
       @Value("${bot.owner}") String ownerName,
-      QuizService quizService,
       UserService userService,
-      MessageService messageService,
-      CategoryService categoryService,
-      ChatMessageStateService chatMessageStateService,
       ObjectMapper objectMapper,
-      SchedulerService schedulerService,
-      MessageSourceService messageSourceService,
-      KeyboardService keyboardService,
-      TelegramClientService telegramClientService,
       TelegramBotService telegramBotService) {
     this.token = token;
     this.ownerName = ownerName;
-
-    this.quizService = quizService;
     this.userService = userService;
     this.objectMapper = objectMapper;
-    this.messageService = messageService;
-    this.categoryService = categoryService;
-    this.schedulerService = schedulerService;
-    this.messageSourceService = messageSourceService;
-    this.keyboardService = keyboardService;
-    this.chatMessageStateService = chatMessageStateService;
-    this.telegramClientService = telegramClientService;
     this.telegramBotService = telegramBotService;
   }
 
