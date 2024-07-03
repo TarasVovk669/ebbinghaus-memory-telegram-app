3612697b53b49b8574e0c30746c244df29c35f62
TarasVovk669 <taras.ivschk@gmail.com>
2024-07-03 20:55:55 +0200

    refactor

---
+++ src/main/java/com/ebbinghaus/memory/app/service/TelegramBotService.java
@@ -0,0 +1,11 @@
+package com.ebbinghaus.memory.app.service;
+
+import com.ebbinghaus.memory.app.model.InputUserData;
+
+public interface TelegramBotService {
+    void processEditMessageCallback(String command, InputUserData inputUserData);
+
+    void processButtonMessageCallback(String command, InputUserData inputUserData);
+
+    void processTextInputCallback(InputUserData inputUserData);
+}
